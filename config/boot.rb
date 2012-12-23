5a7edec62e6771471f68ba0c6e1f894ee21a665c
Teddy Wu <Teddy@WuAndLin.com>
2012-12-22 23:47:46 -0800

    added NLS_LANG to config/boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,6 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+# set nls_lang to avoid warning msgs that fallback to ascii7
+ENV['NLS_LANG'] = 'AMERICAN_AMERICA.AL32UTF8'
