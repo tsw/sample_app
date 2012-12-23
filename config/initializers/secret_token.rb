f33520afad1baf7fc8f665e8f850edeaa2da33dc
Teddy Wu <Teddy@WuAndLin.com>
2012-12-22 22:52:42 -0800

    Initial rail setup with oracle_enhanced adapter and skip-test-unit... can be used for future oracle backed projects

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '798b00bd8679f8fc9f9bc75c9af454070b9ed0ca97c4ce62d0869158d1806d451eb82011d6295a79ae31e906148a6b2a8b909235b488e329a24024c33142a2ce'
