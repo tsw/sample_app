3dec3512992bd25139c5e21a377bbf5515445725
Teddy Wu <Teddy@WuAndLin.com>
2012-12-25 20:32:20 -0800

    finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+	# returns the gravatar for a given user
+	def gravatar_for(user)
+		# pass in lowercase email since MD5 is based on lowercase
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		gravatar_url = "https//secure.gravatar.com/avatar/#{gravatar_id}"
+		#return the image image_tag
+		image_tag(gravatar_url, alt: user.name, class: "gravatar")
+	end
 end
